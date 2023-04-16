f74a0e66882e21dc87b95f5440846982fdf6611c
Christin <caa8062@rit.edu>
2023-04-16 02:49:56 -0400

    Importing module parent attempt 2

--- capstone/nf-scripts/convert-pcap-to-nf.py
+++ capstone/nf-scripts/convert-pcap-to-nf.py
@@ -1,12 +1,17 @@
 import sys, os
 from pathlib import Path
 import subprocess
-from capstone import generatePCAPwoPayload
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+import generatePCAPwoPayload
 
 def main():
     #walk_dir = '{}/'.format(basedir.baseDir)
     walk_dir = '/mnt/capstone_pcaps/'
-    fileList = generateFileList(walk_dir)
+    fileList = generatePCAPwoPayload.generateFileList(walk_dir)
     for file in fileList:
         currentDir = os.path.dirname(file)
         fileName = (os.path.basename(file)).split('.')[0]
@@ -16,4 +21,5 @@ def main():
         subprocess.run('nfpcapd', '-r', f'{file}', '-l', f"{currentDir}/netflows/{fileName}/")
         subprocess.run('nfdump', '-R', f"{currentDir}/netflows/{fileName}", '-o', 'extended', '-o', 'csv', '>', f'{fileName}.csv')
 
+if __name__ == "__main__":
     main()
